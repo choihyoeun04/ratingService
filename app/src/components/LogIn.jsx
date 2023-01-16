97aec92ad66a766ed7607d1c589055d4dd4b56e6
choihyoeun04 <choihyoeun04@gmail.com>
2023-01-15 22:36:34 -0500

    last changes for 1st demo

--- app/src/components/LogIn.jsx
+++ app/src/components/LogIn.jsx
@@ -20,19 +20,24 @@ function LogIn() {
 
   return (
     <div className="loginContainer">
+      <p className="loginTitle">Log in</p>
+        <div className="loginInputContainer">
         <input
           onChange={handleChange}
           name="email"
           value={contact.email}
           placeholder="Email"
+          className="loginInput"
         />
         <input
           onChange={handleChange}
           name="password"
           value={contact.password}
           placeholder="Password"
+          className="loginInput"
         />
-        <button>Log in</button>
+        <button className="loginButton">Log in</button>
+        </div>
     </div>
   );
 }
